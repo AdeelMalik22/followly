2b138ca21722467c199ce6df9d66a743f6c184d8
Subhan Adeel <ms1640070@gmail.com>
2026-08-27 17:14:43 +0500

    added login and signup pages

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from app.api.knowledge import router as knowledge_router
 from app.api.whatsapp import router as whatsapp_router
 from app.api.calendar import router as calendar_router
 from app.api.chat import router as chat_router
+from app.api.pages import router as pages_router
 
 app = FastAPI(title="Followly API", version="1.0.0")
 
@@ -21,6 +22,7 @@ app.include_router(knowledge_router)
 app.include_router(whatsapp_router)
 app.include_router(calendar_router)
 app.include_router(chat_router)
+app.include_router(pages_router)
 
 @app.get("/")
 def read_root():
